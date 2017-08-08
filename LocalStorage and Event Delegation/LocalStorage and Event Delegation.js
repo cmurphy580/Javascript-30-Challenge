7c8fc493be3b3beb2a39830a89b1e5c6f8fac96c
Conor Murphy <cmurphy580@gmail.com>
2017-08-08 00:26:09 -0700

    Update LocalStorage and Event Delegation.js

--- LocalStorage and Event Delegation/LocalStorage and Event Delegation.js
+++ LocalStorage and Event Delegation/LocalStorage and Event Delegation.js
@@ -11,10 +11,12 @@ function addItem(event) {
 	const text = (this.querySelector('[name=item]')).value
 	//can use 'document.querySelector()', but use instead, 'this', which is === to the form, because dont want to select at the global level when we can narrow down our search 
 	//'text' will give up an input and inputs have a value method.
+ 
 	const item = {
 		//text: text, == can use shorthand for this aspect of the object (ES6)
 		text, 
-		done: false 
+		done: false, 
+		time: Date.now()
 	}
 	items.push(item); //every time we push an item to the array, 
 	populateList(items, itemsList); // this function will run which will create a list item and populate list.
@@ -24,12 +26,21 @@ function addItem(event) {
 
 function populateList(tasks = [], tasksList) {  //set the array parameter equal to an empty array(items/tasks), in case you forget to pass something(default parameter) 
 	tasksList.innerHTML = tasks.map((task, i) => {
-		return `
+		if (task.done) {
+			return `
 		<li>
-			<input type="checkbox" data-index=${i} id="item${i}" ${task.done ? 'checked' : ''}><i class="${task.done ? 'fa fa-check-square' : 'fa fa-minus-square-o'}" aria-hidden="true"></i></input>
-			<label for="item${i}"> ${task.text} </label>
+			<input type="checkbox" data-index=${i} id="item${i}" ${'checked'}><i class="${'fa fa-check-square'}" aria-hidden="true"></i></input>
+			<label for="item${i}"> ${task.text} <p>${task.time}</p></label>
 		</li>
-		`;  //with the 'id' for the input and the 'for' for the label being exactly the same, you can click the label and the checkbox will check 
+		`;
+		} else {
+			return `
+		<li>
+			<input type="checkbox" data-index=${i} id="item${i}" ${''}><i class="${'fa fa-minus-square-o'}" aria-hidden="true"></i></input>
+			<label for="item${i}"> ${task.text}</label> <div><p>${task.time}</p><i class="fa fa-clock-o" aria-hidden="true"></i></div>
+		</li>
+		`;
+		}//with the 'id' for the input and the 'for' for the label being exactly the same, you can click the label and the checkbox will check 
 	}).join(''); //convert the array into a string
 }
 
@@ -39,6 +50,15 @@ function toggleDone(event) {
 	const el = event.target;
 	const index = el.dataset.index;
 	items[index].done = !items[index].done; //change the prop
+	/*time completed*/
+	const newTime = Date.now();
+	let x = ~~((newTime - items[index].time) / 1000); 
+	let seconds = ~~((newTime - items[index].time) / 1000) < 60 ? ~~((newTime - items[index].time) / 1000) : ~~((newTime - items[index].time) / 1000) % 60; 
+	let minutes = ~~(x / 60) < 60 ? ~~(x / 60) : ~~(x / 60) % 60; 
+	let y = ~~(x / 60);
+	let hours = ~~(y/ 60)
+	items[index].time = `${hours}:${minutes < 10 ? '0' :''}${minutes}:${seconds < 10 ? '0' :''}${seconds}`;  
+
 	localStorage.setItem('items', JSON.stringify(items)); //save to local storage 
 	populateList(items, itemsList); // update whats on the page
 }
@@ -58,19 +78,4 @@ addItems.addEventListener('submit', addItem);
 itemsList.addEventListener('click', toggleDone); //event delegation = the 'ul' element will be on the page at the time the event is triggered, which in turn pass the event on to one of its child elements.
 reset.addEventListener('click', resetList)
 
-
 populateList(items, itemsList);  //populate onLoad
-
-
-
-
-
-
-
-
-
-
-
-
-
-
